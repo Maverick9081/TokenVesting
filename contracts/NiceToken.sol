45e15d9ead2b670fea136dbbde9d655145b40be7
Maverick9081 <faliyaabhishek@gmail.com>
2022-04-05 16:02:27 +0530

    made it 18 decimals

--- contracts/NiceToken.sol
+++ contracts/NiceToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract NiceToken is ERC20 {
