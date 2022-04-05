bc48931a4aedff0efe3904128f39395745b5e30d
Maverick9081 <faliyaabhishek@gmail.com>
2022-04-05 18:30:15 +0530

    Added network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+require("./scripts/deploy.js");
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY,ETHERSCAN_API_KEY} = process.env;
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.4",
+  defaultNetwork : "rinkeby",
+  networks : {
+    rinkeby : {
+      url : `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts : [`0x${ACCOUNT_PRIVATE_KEY}`]
+    }
+  },
+  etherscan : {
+    apiKey: ETHERSCAN_API_KEY,
+  }
 };
